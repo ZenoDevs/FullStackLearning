20409d9459a58ff2d2617e5ebb9e808a2bb6516e
Edoardo D'Onghia <edoardo.dng@gmail.com>
2025-10-20 20:46:01 +0200

    Little Update

---
+++ Full Stack Meta Course/5 - arrays.js
@@ -0,0 +1,17 @@
+//arrays are objects
+
+var fruits = [];
+fruits.push("apple"); // ['apple']
+fruits.push('pear'); // ['apple', 'pear']
+
+//To remove the last element you can use pop()
+
+fruits.pop();
+console.log(fruits); // ['apple']
+
+//INSIGHT
+
+/* 
+in the exercise you can create functions to build up arrays and then instead or printing them
+you can return them to further manipulations
+*/
